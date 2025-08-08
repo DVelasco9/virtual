66bb659f04a84da1fa1f04e14e31fb43c5cbd25d
DVelasco9 <d12835571@gmail.com>
2025-08-08 01:57:38 -0700

    RQ010: MENU DE PAUSA

--- scripts/muerte.gd
+++ scripts/muerte.gd
@@ -4,7 +4,9 @@ extends Control
 func _on_retry_pressed() -> void:
 	pass
 
-
-
 func _on_back_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_game_over_pressed() -> void:
+	pass
+	#get_tree().change_scene_to_file("res://scenes/EE.tscn")
---
+++ scripts/player_info.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+@onready var menupopup : Node2D = $menuPopup
+
+func _ready() -> void:
+	menupopup.visible = false
+	
+	
+	
+
+func _on_menu_button_pressed() -> void:
+	get_tree().paused = true
+	menupopup.visible = get_tree().paused
+
+func _on_continue_pressed() -> void:
+	get_tree().paused = false
+	menupopup.visible = get_tree().paused
+
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+
+func _on_backmenu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
