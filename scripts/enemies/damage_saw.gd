f82e4f341ddca99debbf89d82873fed301746098
DVelasco9 <d12835571@gmail.com>
2025-08-08 03:12:29 -0700

    RQ04: ENEMIGOS

--- scripts/enemies/damage_saw.gd
+++ scripts/enemies/damage_saw.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
-#class_name damage
+class_name damage
 
 @export var hurt: int
-
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
---
+++ scripts/enemies/saw.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+const SPEED = 200
+const ray_floor_x = 18
+const ray_wall_target_position_x = 13
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready() -> void:
+	velocity.x = SPEED
+	$Ray_floor_detection.position.x = ray_floor_x
+	$Ray_wall_detection.target_position.x = ray_wall_target_position_x
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if not $Ray_floor_detection.is_colliding() or $Ray_wall_detection.is_colliding():
+		velocity.x *= -1
+		$Ray_floor_detection.position.x *= -1
+		$Ray_wall_detection.target_position.x *= -1
+	
+	
+	move_and_slide()
